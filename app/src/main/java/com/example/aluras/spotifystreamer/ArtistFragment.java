0bd27be50d59e28c4b2f9a876b266f0a9c562ade
Andre <andrelrs80@gmail.com>
2015-06-18 00:42:08 -0300

    implementação do comando de busca

--- app/src/main/java/com/example/aluras/spotifystreamer/ArtistFragment.java
+++ app/src/main/java/com/example/aluras/spotifystreamer/ArtistFragment.java
@@ -1,13 +1,18 @@
 package com.example.aluras.spotifystreamer;
 
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -16,6 +21,8 @@ import java.util.ArrayList;
  * A simple {@link Fragment} subclass.
  */
 public class ArtistFragment extends Fragment {
+
+    private  ArrayAdapter<String> mArtistAdapter;
 
 
     public ArtistFragment() {
@@ -29,20 +36,56 @@ public class ArtistFragment extends Fragment {
         // Inflate the layout for this fragment
         View rootView =  inflater.inflate(R.layout.fragment_artist, container, false);
 
+        ((TextView)rootView.findViewById(R.id.txtBuscaArtista)).setOnEditorActionListener(new EditText.OnEditorActionListener(){
+            @Override
+            public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
+                if (i == EditorInfo.IME_ACTION_SEARCH){
+                    updateArtists();
+                    return true;
+                }
+
+                return false;
+            }
+        });
+
         ArrayList<String> dados = new ArrayList<String>();
 
-        dados.add("Titãs");
-        dados.add("Engenhiros do Hawai");
+        //dados.add("Titãs");
+        //dados.add("Engenhiros do Hawai");
 
-        ArrayAdapter<String> artistAdapter = new ArrayAdapter<String>(getActivity(), R.layout.list_item_artist, R.id.txtArtistName,dados);
+        mArtistAdapter = new ArrayAdapter<String>(getActivity(), R.layout.list_item_artist, R.id.txtArtistName,dados);
 
         ListView listView =(ListView) rootView.findViewById(R.id.listViewArtist);
 
-        listView.setAdapter(artistAdapter);
-
-
+        listView.setAdapter(mArtistAdapter);
 
         return rootView;
+    }
+
+    private void updateArtists(){
+        new FetchArtistTask().execute();
+    }
+
+    public class FetchArtistTask extends AsyncTask<Void,Void,String[]>{
+        
+        @Override
+        protected String[] doInBackground(Void... voids) {
+            
+            String[] dados = {"Titãs", "Engenheiros do Hawai"};
+            
+            return dados;
+        }
+
+        @Override
+        protected void onPostExecute(String[] strings) {
+            if (strings != null){
+                mArtistAdapter.clear();
+                for(String dayForecastStr : strings){
+                    mArtistAdapter.add(dayForecastStr);
+                }
+            }
+
+        }
     }
 
 
