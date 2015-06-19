f56c803b2b247d376e7a2eddcc2af08e3fbc78c6
Andre <andrelrs80@gmail.com>
2015-06-19 00:48:41 -0300

    chamada da API do Spotify

--- app/src/main/java/com/example/aluras/spotifystreamer/ArtistFragment.java
+++ app/src/main/java/com/example/aluras/spotifystreamer/ArtistFragment.java
@@ -4,6 +4,7 @@ package com.example.aluras.spotifystreamer;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,6 +16,14 @@ import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+
+import kaaes.spotify.webapi.android.SpotifyApi;
+import kaaes.spotify.webapi.android.SpotifyService;
+import kaaes.spotify.webapi.android.models.Artist;
+import kaaes.spotify.webapi.android.models.Artists;
+import retrofit.Callback;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
 
 
 /**
@@ -68,10 +77,26 @@ public class ArtistFragment extends Fragment {
         
         @Override
         protected String[] doInBackground(Void... voids) {
+
+            final ArrayList<String> dados = new ArrayList<String>();
+
+            SpotifyApi api = new SpotifyApi();
+            SpotifyService spotify = api.getService();
+            spotify.getArtists("Engenheiros", new Callback<Artists>() {
+                @Override
+                public void success(Artists artists, Response response) {
+                    for (Artist artist : artists.artists){
+                        dados.add(artist.name);
+                    }
+                }
+
+                @Override
+                public void failure(RetrofitError error) {
+
+                }
+            });
             
-            String[] dados = {"Titãs", "Engenheiros do Hawai"};
-            
-            return dados;
+            return dados.toArray(new String[dados.size()]);
         }
 
         @Override
